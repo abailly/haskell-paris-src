89e310a48cf8c1d12647bc2f485d69ddbcf07d84
lucas dicioccio <lucas@dicioccio.fr>
2013-01-06 12:52:42 +0100

    add fork-me-on-github banner

--- Views/Layout.hs
+++ Views/Layout.hs
@@ -13,6 +13,7 @@ layout f subtitle = [hamlet|
 <head>
     ^{headblock}
 <body>
+    <a href="https://github.com/lucasdicioccio/haskell-paris-src"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
     <div class="container">
         <div class="row-fluid span12">
             ^{header subtitle}
